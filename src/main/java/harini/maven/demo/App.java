8bb8b84fb0a29783e002eb571abec38444d0a6e1
swakesh puligilla <swakesh.puligilla@fmr.com>
2017-02-26 19:32:38 -0500

    adding maven project

---
+++ src/main/java/harini/maven/demo/App.java
@@ -0,0 +1,13 @@
+package harini.maven.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
